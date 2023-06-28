617ec73be40c7d059b7f2b7b625ad8249ada7c60
spande-cpu <shashwatpande101@gmail.com>
2023-06-28 17:00:06 +0100

    Added .md files.

--- README.md
+++ README.md
@@ -1,3 +1,100 @@
-# LOTRlang
-Testing a k-gram model
+Forum Scraper for able2know.org
+================
 
+# Description
+
+This code is a scraper written in R Markdown for extracting information
+from the philosophy forum on the website
+[able2know.org](https://able2know.org). The output format is set to
+`github_document`.
+
+## Required Libraries
+
+``` r
+library(tidyverse)
+```
+
+    ## ── Attaching core tidyverse packages ──────────────────────── tidyverse 2.0.0 ──
+    ## ✔ dplyr     1.1.2     ✔ readr     2.1.4
+    ## ✔ forcats   1.0.0     ✔ stringr   1.5.0
+    ## ✔ ggplot2   3.4.2     ✔ tibble    3.2.1
+    ## ✔ lubridate 1.9.2     ✔ tidyr     1.3.0
+    ## ✔ purrr     1.0.1     
+    ## ── Conflicts ────────────────────────────────────────── tidyverse_conflicts() ──
+    ## ✖ dplyr::filter() masks stats::filter()
+    ## ✖ dplyr::lag()    masks stats::lag()
+    ## ℹ Use the conflicted package (<http://conflicted.r-lib.org/>) to force all conflicts to become errors
+
+``` r
+library(rvest)
+```
+
+    ## 
+    ## Attaching package: 'rvest'
+    ## 
+    ## The following object is masked from 'package:readr':
+    ## 
+    ##     guess_encoding
+
+The code begins by loading the necessary libraries, namely `tidyverse`
+and `rvest`. The `tidyverse` library provides a set of packages for data
+manipulation and visualization, while `rvest` is used for web scraping
+in R.
+
+## Function: get_info
+
+``` r
+get_info <- function(base_url)
+{
+  # Code omitted for brevity
+}
+```
+
+This function, `get_info`, takes a base URL as input and is responsible
+for extracting topics, URLs, and metadata from the forum page.
+
+## Function: get_discussion
+
+``` r
+get_discussion <- function(thread_url)
+{
+  # Code omitted for brevity
+}
+```
+
+The `get_discussion` function takes a thread URL as input and extracts
+the message content and discussions from that particular thread.
+
+## Function: scrape_a2k
+
+``` r
+scrape_a2k <- function(page_url="https://able2know.org/forum/philosophy/", n_pages = 20)
+{
+  # Code omitted for brevity
+}
+```
+
+The `scrape_a2k` function is the main crawler function. It takes a page
+URL (defaulting to the philosophy forum on able2know.org) and the number
+of pages to scrape as input. It then performs the web scraping process
+by calling the `get_info` and `get_discussion` functions.
+
+## Execute the Scraper
+
+To run the web scraping process and retrieve the data, uncomment the
+following code:
+
+``` r
+data <- scrape_a2k(n_pages = 40)
+```
+
+By uncommenting this line, you can run the `scrape_a2k` function and
+specify the number of pages to scrape (in this case, 40). The extracted
+data will be stored in the `data` variable.
+
+Please note that running the scraper may take some time, depending on
+the number of pages to scrape and the website’s responsiveness.
+
+That’s it! This code provides a framework for scraping information from
+the philosophy forum on able2know.org using R Markdown. Feel free to
+modify it according to your needs. \`\`\`
--- code/a2kCrawler.R
+++ code/a2kCrawler.R
@@ -4,52 +4,50 @@ library(tidyverse)
 library(rvest)
 
 # Get topics, urls and metadata
-get_info <- function(base_url)
-{
+get_info <- function(base_url) {
   base_html <- read_html(base_url)
   ## Get thread index data
-  Url <- base_html %>% 
-    html_nodes(".gridItem") %>% 
-    html_nodes(".title") %>% 
-    html_node("a") %>% 
+  Url <- base_html %>%
+    html_nodes(".gridItem") %>%
+    html_nodes(".title") %>%
+    html_node("a") %>%
     html_attr("href")
-  
-  Titles <- base_html %>% 
-    html_nodes(".gridItem") %>% 
-    html_nodes(".title") %>% 
+
+  Titles <- base_html %>%
+    html_nodes(".gridItem") %>%
+    html_nodes(".title") %>%
     html_text2()
-  
-  Score <- base_html %>% 
-    html_nodes(".gridItem") %>% 
-    html_nodes(".postScore") %>% 
+
+  Score <- base_html %>%
+    html_nodes(".gridItem") %>%
+    html_nodes(".postScore") %>%
     html_text2()
   ## Get Meta-Data
-  meta_data <- base_html %>% 
-    html_nodes(".gridItem") %>% 
+  meta_data <- base_html %>%
+    html_nodes(".gridItem") %>%
     html_nodes(".topicMeta ") %>%
     html_text2() %>%
-    as_tibble() %>% 
+    as_tibble() %>%
     separate(
-      "value", 
+      "value",
       c("Topics", "OriginalPoster", "PostDate", "Replies", "Views", "LastReply"),
       sep = "\n"
     ) %>%
     mutate(
       Topics = str_remove(Topics, "Forums: "),
       OriginalPoster = str_remove(OriginalPoster, "Question by |Discussion by "),
-      PostDate = str_remove(PostDate, "Posted "), 
+      PostDate = str_remove(PostDate, "Posted "),
       Replies = str_remove_all(str_remove(Replies, "Replies: "), ","),
       Views = str_remove_all(str_remove(Views, "Views: "), ","),
       LastReply = str_remove(LastReply, "Last Post by ")
     ) %>%
     separate("LastReply", c("LastReplyFrom", "LastReplyAt"), sep = "on ")
-  
+
   cbind(Url, Titles, Score, meta_data)
 }
 
 # Get message content and discussions
-get_discussion <- function(thread_url)
-{
+get_discussion <- function(thread_url) {
   thread_url %>%
     read_html() %>%
     html_nodes(".expandedPostBody") %>%
@@ -59,30 +57,29 @@ get_discussion <- function(thread_url)
 }
 
 # Crawler
-scrape_a2k <- function(page_url="https://able2know.org/forum/philosophy/", n_pages = 20)
-{
+scrape_a2k <- function(page_url = "https://able2know.org/forum/philosophy/", n_pages = 20) {
   seed_url <- page_url
   seed_html <- read_html(seed_url)
   # Total No. of Pages in the Forum
   total_pages <- seed_html %>%
-    html_nodes(".box.pagination") %>% 
-    html_text2() %>% 
-    str_replace(",", "") %>% 
+    html_nodes(".box.pagination") %>%
+    html_text2() %>%
+    str_replace(",", "") %>%
     str_extract_all(., "[0-9]+") %>%
     unlist() %>%
-    as.numeric() %>% 
+    as.numeric() %>%
     max(na.rm = T)
   # No, of Pages to Scrape
   if (n_pages == "all" & total_pages > 1) {
-    crawler_depth = 2:total_pages
-  } else if(!is.numeric(n_pages) & n_pages != "all") {
+    crawler_depth <- 2:total_pages
+  } else if (!is.numeric(n_pages) & n_pages != "all") {
     print(
       "Error: Please supply the number of pages to scrape or set n_pages = 'all' to scrape all pages in the forum (this may take some time)."
-      )
-  } else if(n_pages > total_pages | n_pages == 1 | n_pages == 0 | n_pages < 0) {
-    crawler_depth = 1
+    )
+  } else if (n_pages > total_pages | n_pages == 1 | n_pages == 0 | n_pages < 0) {
+    crawler_depth <- 1
   } else {
-    crawler_depth = 2:n_pages
+    crawler_depth <- 2:n_pages
   }
   # Page urls to scrape content from
   if (isTRUE(crawler_depth == 1) | !is.numeric(crawler_depth) | isTRUE(is.infinite(crawler_depth))) {
@@ -93,17 +90,12 @@ scrape_a2k <- function(page_url="https://able2know.org/forum/philosophy/", n_pag
       paste0(seed_url, "page-", crawler_depth)
     )[crawler_depth] %>% .[complete.cases(.)]
   }
-  
+
   master <- purrr::map_dfr(page_urls, get_info)
-  
+
   master %>%
     mutate(posts = Url %>% purrr::map(get_discussion))
-  
 }
 
 # Get Data: Uncomment to run.
 # data <- scrape_a2k(n_pages = 40)
-
-
-  
-  
